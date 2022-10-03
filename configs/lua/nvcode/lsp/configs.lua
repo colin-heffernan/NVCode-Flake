beda3a6ee087ec6e0f434768e1edfe90ba69ac9a
Colin Heffernan <colinpheffernan@gmail.com>
2022-10-03 00:53:34 -0400

    Fix a typo in LSP settings

--- configs/lua/nvcode/lsp/configs.lua
+++ configs/lua/nvcode/lsp/configs.lua
@@ -11,7 +11,7 @@ for _, server in pairs(servers) do
 		on_attach = require("nvcode.lsp.handlers").on_attach,
 		capabilities = require("nvcode.lsp.handlers").capabilities,
 	}
-	local has_custom_opts, server_custom_opts = pcall(require, "nvcode.lsp.settings" .. server)
+	local has_custom_opts, server_custom_opts = pcall(require, "nvcode.lsp.settings." .. server)
 	if has_custom_opts then
 		opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
 	end
