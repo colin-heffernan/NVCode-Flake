59a0d0b2024cd3d974274feb65b239894eac91a4
Colin Heffernan <colinpheffernan@gmail.com>
2022-12-21 18:06:36 -0500

    Only use TS parser

--- lib/buildPlugin.nix
+++ lib/buildPlugin.nix
@@ -2,26 +2,26 @@
 	inherit (prev.vimUtils) buildVimPluginFrom2Nix;
 
 	treesitterGrammars = prev.tree-sitter.withPlugins (p: [
-		p.tree-sitter-bash
-		p.tree-sitter-c
-		p.tree-sitter-cpp
-		p.tree-sitter-css
-		p.tree-sitter-haskell
-		p.tree-sitter-html
-		p.tree-sitter-javascript
-		p.tree-sitter-json
-		p.tree-sitter-latex
-		p.tree-sitter-lua
-		p.tree-sitter-markdown
-		p.tree-sitter-markdown-inline
-		p.tree-sitter-nix
-		p.tree-sitter-norg
-		p.tree-sitter-python
-		p.tree-sitter-regex
-		p.tree-sitter-rust
-		p.tree-sitter-svelte
+		# p.tree-sitter-bash
+		# p.tree-sitter-c
+		# p.tree-sitter-cpp
+		# p.tree-sitter-css
+		# p.tree-sitter-haskell
+		# p.tree-sitter-html
+		# p.tree-sitter-javascript
+		# p.tree-sitter-json
+		# p.tree-sitter-latex
+		# p.tree-sitter-lua
+		# p.tree-sitter-markdown
+		# p.tree-sitter-markdown-inline
+		# p.tree-sitter-nix
+		# p.tree-sitter-norg
+		# p.tree-sitter-python
+		# p.tree-sitter-regex
+		# p.tree-sitter-rust
+		# p.tree-sitter-svelte
 		p.tree-sitter-typescript
-		p.tree-sitter-vim
+		# p.tree-sitter-vim
 	]);
 
 	buildPlug = name: buildVimPluginFrom2Nix {
