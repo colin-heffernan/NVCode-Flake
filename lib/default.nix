1bae5847e9160b0a8842c45f4a3d67c94b86b857
Colin Heffernan <colinpheffernan@gmail.com>
2023-03-04 16:33:15 -0500

    Bugfixing TS x3

--- lib/default.nix
+++ lib/default.nix
@@ -2,5 +2,5 @@
 {
 	inherit (pkgs.lib);
 	neovimBuilder = import ./neovimBuilder.nix { inherit pkgs; };
-	buildPluginOverlay = import ./buildPlugin.nix { inherit pkgs inputs plugins; };
+	buildPluginOverlay = import ./buildPlugin.nix { inherit pkgs inputs plugins grammars; };
 }
