df17bf57fda751b77b317e6aeff2c5820c6c4ece
Colin Heffernan <colinpheffernan@gmail.com>
2023-03-04 17:11:05 -0500

    Bugfixing TS x17

--- lib/override.nix
+++ lib/override.nix
@@ -1,4 +1,4 @@
-{ pkgs }: final: prev: {
+{ pkgs, inputs, grammars }: final: prev: {
 	nvim-treesitter = prev.nvim-treesitter.overrideAttrs (old:
 		pkgs.callPackage ./treesitterOverrides.nix {} final prev
 	);
