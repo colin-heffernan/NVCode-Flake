bbca9e30ece5b320e1e2da3f6f44cd0ddc1e9489
Colin Heffernan <colinpheffernan@gmail.com>
2023-03-04 14:07:10 -0500

    Temporarily switch to Nixpkgs plugins

--- lib/neovimBuilder.nix
+++ lib/neovimBuilder.nix
@@ -6,7 +6,7 @@
 		lua = pkgs.lua5_1;
 	};
 	wrapNvcode = neovim-unwrapped: lib.makeOverridable (nvcodeUtils.legacyWrapper neovim-unwrapped);
-	startPlugins = with pkgs.neovimPlugins; [
+	startPlugins = with pkgs.vimPlugins; [
 		plenary-nvim
 		nui-nvim
 		popup-nvim
@@ -20,35 +20,35 @@
 		neo-tree-nvim
 		telescope-nvim
 		project-nvim
-		nvim-treesitter						# For building plugins from source
-		# (nvim-treesitter.withPlugins (plugins: with plugins; [		# For using Nixpkgs plugins
-		# 	bash
-		# 	c
-		# 	cpp
-		# 	css
-		# 	dockerfile
-		# 	gdscript
-		# 	# gitignore
-		# 	graphql
-		# 	haskell
-		# 	html
-		# 	javascript
-		# 	json
-		# 	latex
-		# 	lua
-		# 	markdown
-		# 	nix
-		# 	norg
-		# 	python
-		# 	regex
-		# 	rust
-		# 	scss
-		# 	svelte
-		# 	toml
-		# 	typescript
-		# 	vim
-		# 	# yuck
-		# ]))
+		# nvim-treesitter						# For building plugins from source
+		(nvim-treesitter.withPlugins (plugins: with plugins; [		# For using Nixpkgs plugins
+			bash
+			c
+			cpp
+			css
+			dockerfile
+			gdscript
+			# gitignore
+			graphql
+			haskell
+			html
+			javascript
+			json
+			latex
+			lua
+			markdown
+			nix
+			norg
+			python
+			regex
+			rust
+			scss
+			svelte
+			toml
+			typescript
+			vim
+			# yuck
+		]))
 		nvim-treesitter-textobjects
 		nvim-ts-autotag
 		nvim-ts-context-commentstring
